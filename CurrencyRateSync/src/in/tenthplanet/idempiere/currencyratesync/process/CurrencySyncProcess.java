afc242f33ca1c3bf705289e42cf2e43a6a29c74e
TEMP_007 <surya.temp05@gmail.com>
2026-04-20 18:49:15 +0530

    Actually added process, service, util modules and main process class
    * Initialize project structure with process, service, util modules and
    main process class
    
    * Actually added project structure with process, service, util modules and
    main process class

---
+++ CurrencyRateSync/src/in/tenthplanet/idempiere/currencyratesync/process/CurrencySyncProcess.java
@@ -0,0 +1,19 @@
+package in.tenthplanet.idempiere.currencyratesync.process;
+
+import org.compiere.process.SvrProcess;
+
+public class CurrencySyncProcess extends SvrProcess {
+
+	@Override
+	protected void prepare() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected String doIt() throws Exception {
+		// TODO Auto-generated method stub
+		return "Surya-CurrencyRateSync";
+	}
+
+}
