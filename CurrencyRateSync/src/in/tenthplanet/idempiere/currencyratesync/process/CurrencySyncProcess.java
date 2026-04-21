e30f9993fa559615e5f38b4c1533669e03632748
TEMP_007 <surya.temp05@gmail.com>
2026-04-21 17:29:39 +0530

    Currency Rate Sync Process with Parameter Handling
    * Added parameter variables and initialized them in prepare() for Currency
    Rate Sync process
    
    * Updated bundle version in META-INF/MANIFEST.MF

--- CurrencyRateSync/src/in/tenthplanet/idempiere/currencyratesync/process/CurrencySyncProcess.java
+++ CurrencyRateSync/src/in/tenthplanet/idempiere/currencyratesync/process/CurrencySyncProcess.java
@@ -1,12 +1,45 @@
 package in.tenthplanet.idempiere.currencyratesync.process;
 
+import org.compiere.model.MProcessPara;
+import org.compiere.process.ProcessInfoParameter;
 import org.compiere.process.SvrProcess;
+import java.sql.Timestamp;
 
+/**
+ *	Currency Rate Sync Process
+ *	
+ *  @author Surya
+ */
 public class CurrencySyncProcess extends SvrProcess {
+	
+	/**	Currency Rate Sync Parameter  */
+	
+	/**Currency					*/
+	private String  		p_C_Currency_ID=null;
+	/**Currency	To				*/
+	private String  		p_C_Currency_ID_To=null;
+	/**Date						*/
+	private Timestamp   	p_Date;
 
-    @Override
+	/**
+	 *  Prepare - e.g., get Parameters.
+	 */
     protected void prepare() {
-        System.out.println("CurrencySyncProcess — prepare() called");
+        ProcessInfoParameter[] para = getParameter();
+        for(int i = 0; i < para.length; i++) {
+        	String name=para[i].getParameterName();
+        	if(para[i].getParameter() == null && para[i].getParameter_To() == null)
+    			;
+        	else if(name.equals("C_Currency_ID"))
+        		p_C_Currency_ID=para[i].getParameterAsString();
+        	else if(name.equals("C_Currency_ID_To"))
+        		p_C_Currency_ID_To=para[i].getParameterAsString();
+        	else if(name.equals("Date"))
+        		p_Date=para[i].getParameterAsTimestamp();
+        	else
+        		MProcessPara.validateUnknownParameter(getProcessInfo().getAD_Process_ID(), para[i]);
+        }
+        
     }
 
     @Override
